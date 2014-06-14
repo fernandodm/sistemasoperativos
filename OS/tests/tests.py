221b198ae67fa715b2b20dc152edf092e44b6dd5
“juanmanuelvallejos” <juanmanuelvallejos2@gmail.com>
2014-06-14 19:03:14 -0300

    ahora se corre test.py y corre todos los tests

--- OS/tests/tests.py
+++ OS/tests/tests.py
@@ -5,13 +5,14 @@ from tclock import ClockTest
 from tcpu import CpuTest
 from tdisc import DiscTest
 from  tfifoQueue import FifoQueueTest
-from tinterruptionHandler HandlerTest
-from tinterruptionProcessor import InterruptionProcesorTest
+from tinterruptionHandler import InterruptionHandlerTest
+from tinterruptionProcessor import InterruptionProcessorTest
 from tkernel import KernelTest
 from tpriorityQueue import PriorityQueueTest
 
-listTests = [ChanceTest,ClockTest,CpuTest,DiscTest,FifoQueueTest,HandlerTest,InterruptionProcesorTest,KernelTest,PriorityQueueTest]
 
-for test in range listTests:
+listTests = [ChanceTest,ClockTest,CpuTest,DiscTest,FifoQueueTest,InterruptionHandlerTest,InterruptionProcessorTest,KernelTest,PriorityQueueTest]
+
+for test in listTests:
 	suite = unittest.TestLoader().loadTestsFromTestCase(test)
 	unittest.TextTestRunner(verbosity=2).run(suite) 
